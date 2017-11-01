eccab3591667419720c23fdf2db8e9c3a942da52
theSonOfMan <petr.galaburda@gmail.com>
2017-11-01 22:28:29 +0300

    hackaton totals

--- web_hackaton/src/main/java/ru/atom/chat/ChatController.java
+++ web_hackaton/src/main/java/ru/atom/chat/ChatController.java
@@ -11,10 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.Set;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
@@ -42,21 +41,30 @@ public class ChatController {
         if (!online.add(name)) {
             return new ResponseEntity<>("Already logged in", HttpStatus.BAD_REQUEST);
         }
-        messages.addFirst("[" + name + "] is online");
+        messages.addFirst("<font face=\"Helvetica\" color=\"green\">[" + name + "]</font><font face=\"Helvetica\"> is online</font>");
         log.info(name + " logged in");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     /**
-     * curl -X POST -i localhost:8080/chat/logout?name=I_AM_STUPID"
+     * curl -X POST -i localhost:8080/chat/logout -d "name=I_AM_STUPID"
      */
     @RequestMapping(
             path = "logout",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity logout(@RequestParam("name") String name) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> logout(@RequestParam("name") String name) {
+        log.info("Entered logout method");
+        if (name == null || name.isEmpty()) {
+            return new ResponseEntity<>("No name provided", HttpStatus.BAD_REQUEST);
+        }
+        if (!online.remove(name)) {
+            return new ResponseEntity<>("No logged user with matching name", HttpStatus.BAD_REQUEST);
+        }
+        messages.addFirst("<font face=\"Helvetica\" color=\"red\">[" + name + "]</font><font face=\"Helvetica\"> is offline</font>");
+        log.info(name + " logged out");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
@@ -68,7 +76,11 @@ public class ChatController {
             method = RequestMethod.GET,
             produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity online() {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        Iterator<String> itr = online.iterator();
+        while (itr.hasNext()) {
+            messages.addFirst("[" + itr.next() + "]");
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
@@ -80,8 +92,32 @@ public class ChatController {
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
+        Date date = new Date();
+        if (!online.contains(name)) {
+            return new ResponseEntity<>("Not logined", HttpStatus.UNAUTHORIZED);
+        }
+        if (msg == null) {
+            return new ResponseEntity<>("No message provided", HttpStatus.BAD_REQUEST);
+        }
+        if (msg.length() > 140) {
+            return new ResponseEntity<>("Too long message", HttpStatus.BAD_REQUEST);
+        }
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("HH:mm:ss ");
+        log.info(formatForDateNow.format(date) + "[" + name + "]: " + msg);
+        messages.addFirst(formatForDateNow.format(date) +"[" + name + "]: " + msg);
+        try {
+            FileWriter writer = new FileWriter("ChatHistory.txt", true);
+            for (String line : messages) {
+                writer.write(line);
+                writer.write(System.getProperty("line.separator"));
+            }
+            writer.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
@@ -91,7 +127,7 @@ public class ChatController {
     @RequestMapping(
             path = "chat",
             method = RequestMethod.GET,
-            produces = MediaType.TEXT_PLAIN_VALUE)
+            produces = MediaType.TEXT_HTML_VALUE)
     public ResponseEntity<String> chat() {
         return new ResponseEntity<>(messages.stream()
                 .map(Object::toString)
